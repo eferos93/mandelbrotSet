fc6051c932409135faaa4bf1480639f246a2996e
Eros <eros.fabrici@gmail.com>
2020-01-16 12:56:28 +0100

    modified hybrid bash script

--- src/run_hybrid_str_scal.sh
+++ src/run_hybrid_str_scal.sh
@@ -1,11 +1,15 @@
 
 echo "Hybrid version"
-module load openmpi
+module purge
+module load openmpi/1.8.3/intel/14.0
+module load impi-trial/5.0.1.035
 cd mandelbrotSet/src
-make
+mpiicc -fopenmp mandelbrot_set.c -o mandelbrot_set_hybrid.x
+export I_MPI_JOB_RESPECT_PROCESS_PLACEMENT=off
+export OMP_PLACES=cores
+export OMP_PROC_BIND=spread
 
-echo "PARALLEL"
-OLDIFS=$IFS; IFS=','; for procs in 1,20 2,10 4,5 10,2 20,1; do IFS=","; set -- $procs;
+OLDIFS=$IFS; IFS=','; for procs in 1,20 2,10 4,5 10,2 20,1; do set -- $procs;
     export OMP_NUM_THREADS=$2
-    mpirun -np $1 ./mandelbrot_set.x 20000 20000 -2.0 -2.0 3.0 5.5 65535;
-done;IFS=$OLDIFS
+    mpiexec.hydra -n $1 -ppn 1 ./mandelbrot_set_hybrid.x 20000 20000 -2.0 -2.0 3.0 5.5 65535;
+done; IFS=$OLDIFS
