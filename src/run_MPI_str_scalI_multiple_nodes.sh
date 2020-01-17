beedf278b35a12f312599ceea4bd9cc8b2159d7d
Eros <eros.fabrici@gmail.com>
2020-01-17 18:41:22 +0100

    renamed executable in the new scripts

--- src/run_MPI_str_scalI_multiple_nodes.sh
+++ src/run_MPI_str_scalI_multiple_nodes.sh
@@ -3,9 +3,9 @@ module purge
 module load openmpi/1.8.3/intel/14.0
 module load impi-trial/5.0.1.035
 cd mandelbrotSet/src
-mpiicc mandelbrot_set.c -o mandelbrot_set_MPI.x
+mpiicc mandelbrot_set.c -o mandelbrot_set_MPI_mult_nodes.x
 
 for procs in `seq 20 20 80`; do
-    mpiexec.hydra -n ${procs} ./mandelbrot_set_MPI.x 20000 20000 -2.0 -1.0 1.0 2.0 65535
+    mpiexec.hydra -n ${procs} ./mandelbrot_set_MPI_mult_nodes.x 20000 20000 -2.0 -1.0 1.0 2.0 65535
 done
     
--- src/run_hybrid_str_scal_multiple_nodes.sh
+++ src/run_hybrid_str_scal_multiple_nodes.sh
@@ -4,12 +4,12 @@ module purge
 module load openmpi/1.8.3/intel/14.0
 module load impi-trial/5.0.1.035
 cd mandelbrotSet/src
-mpiicc -fopenmp mandelbrot_set.c -o mandelbrot_set_hybrid.x
+mpiicc -fopenmp mandelbrot_set.c -o mandelbrot_set_hybrid_mult_nodes.x
 export I_MPI_JOB_RESPECT_PROCESS_PLACEMENT=off
 export OMP_PLACES=cores
 export OMP_PROC_BIND=spread
 
 OLDIFS=$IFS; IFS=','; for procs in 1,20 2,20 3,20 4,20; do set -- $procs;
     export OMP_NUM_THREADS=$2
-    mpiexec.hydra -n $1 -ppn 1 ./mandelbrot_set_hybrid.x 20000 20000 -2.0 -1.0 2.0 1.0 65535;
+    mpiexec.hydra -n $1 -ppn 1 ./mandelbrot_set_hybrid_mult_nodes.x 20000 20000 -2.0 -1.0 2.0 1.0 65535;
 done; IFS=$OLDIFS
