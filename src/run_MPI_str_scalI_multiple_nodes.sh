9354a3608ef26a874cb0e58470c497798e06f555
Eros <eros.fabrici@gmail.com>
2020-01-17 18:39:22 +0100

    added two new scripts for tests on more than one node

---
+++ src/run_MPI_str_scalI_multiple_nodes.sh
@@ -0,0 +1,11 @@
+echo "MPI version"
+module purge
+module load openmpi/1.8.3/intel/14.0
+module load impi-trial/5.0.1.035
+cd mandelbrotSet/src
+mpiicc mandelbrot_set.c -o mandelbrot_set_MPI.x
+
+for procs in `seq 20 20 80`; do
+    mpiexec.hydra -n ${procs} ./mandelbrot_set_MPI.x 20000 20000 -2.0 -1.0 1.0 2.0 65535
+done
+    
