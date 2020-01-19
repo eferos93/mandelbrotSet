bf1aa4f61540cba6bfa11110f75f2eb4a0cd38f2
Eros <eros.fabrici@gmail.com>
2020-01-19 14:51:15 +0100

    modified scripts for serial execution

--- src/UlyBETA_run_serial.sh
+++ src/UlyBETA_run_serial.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd mandelbrotSet/src
+module load gnu7/7.3.1
 gcc mandelbrot_set_serial.c -o mandelbrot_set_serial.x
 echo "SERIAL"
 ./mandelbrot_set_serial.x 4000 4000 -2.0 -1.0 1.0 1.0 65535
--- src/UlyBETA_run_serial_weak_scal.sh
+++ src/UlyBETA_run_serial_weak_scal.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd mandelbrotSet/src
+module load gnu7/7.3.1
 gcc mandelbrot_set_serial.c -o mandelbrot_set_serial.x
 echo "SERIAL"
 for input in 500 707 1000 1225 1421 1579; do
