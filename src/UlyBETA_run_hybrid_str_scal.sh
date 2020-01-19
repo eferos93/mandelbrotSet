9263fe85f7acb5b30f7cb847251012dfaf3b9c6a
Eros <eros.fabrici@gmail.com>
2020-01-19 12:58:18 +0100

    modified input sizes in scripts

--- src/UlyBETA_run_hybrid_str_scal.sh
+++ src/UlyBETA_run_hybrid_str_scal.sh
@@ -12,5 +12,5 @@ export OMP_PROC_BIND=spread
 
 OLDIFS=$IFS; IFS=','; for procs in 1,20 4,5 10,2 20,1; do set -- $procs;
     export OMP_NUM_THREADS=$2
-    mpirun -np $1 ./mandelbrot_set_hybrid.x 2000 2000 -2.0 -1.0 1.0 1.0 65535;
+    mpirun -np $1 ./mandelbrot_set_hybrid.x 3000 3000 -2.0 -1.0 1.0 1.0 65535;
 done; IFS=$OLDIFS
--- src/UlyBETA_run_serial.sh
+++ src/UlyBETA_run_serial.sh
@@ -2,4 +2,4 @@
 cd mandelbrotSet/src
 gcc mandelbrot_set_serial.c -o mandelbrot_set_serial.x
 echo "SERIAL"
-./mandelbrot_set_serial.x 2000 2000 -2.0 -1.0 1.0 1.0 65535
+./mandelbrot_set_serial.x 3000 3000 -2.0 -1.0 1.0 1.0 65535
