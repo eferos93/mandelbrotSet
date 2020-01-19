1b54b5f6037e0da1fe98616fce72693133a092b3
Eros <eros.fabrici@gmail.com>
2020-01-19 18:41:18 +0100

    new script for hybrid str/weak scal

--- src/UlyBETA_run_hybrid_weak_s.sh
+++ src/UlyBETA_run_hybrid_weak_s.sh
@@ -9,7 +9,7 @@ mpicc -fopenmp mandelbrot_set.c -o mandelbrot_set_hybrid.x
 export OMP_PLACES=cores
 export OMP_PROC_BIND=spread
 
-OLDIFS=$IFS; IFS=','; for procs in 1,2 2,4 2,8 3,8 5,6 5,8; do set -- $procs;
+OLDIFS=$IFS; IFS=','; for procs in 1,2,1000 2,4,1414 2,8,2000 3,8,2828 5,6,3464 5,8,4000; do set -- $procs;
     export OMP_NUM_THREADS=$2
     mpirun -np $1 ./mandelbrot_set_hybrid.x 4000 4000 -2.0 -1.0 1.0 1.0 65535;
 done; IFS=$OLDIFS
