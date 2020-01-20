3f79def11d08fa90a5b3500674b3de8f076fd7ca
Eros Fabrici <eros.fabrici@gmail.com>
2020-01-20 10:49:35 +0100

    ciao

--- src/run_serial_weak_scal.sh
+++
@@ -1,6 +0,0 @@
-cd $PBS_O_WORKDIR
-gcc mandelbrot_set_serial.c -o mandelbrot_set_serial.x -lrt
-echo "SERIAL"
-for procs in 500 707 1000 1225 1421 1579; do
-    ./mandelbrot_set_serial.x ${procs} ${procs} -2.0 -1.0 1.0 1.0 65535
-done
